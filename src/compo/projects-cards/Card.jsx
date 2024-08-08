918bbd405b106fbe566b2b7d62450c093a4d267c
Jaimin Mevada <jaiminkumarmevada@gmail.com>
2024-08-08 12:27:52 -0800

    bg image set

--- src/compo/projects-cards/Card.jsx
+++ src/compo/projects-cards/Card.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './card.css';
+import backgroundImage from './project-images/weatherproimage.png'; 
 
 function Card() {
   return (
-    <div className="card">card</div>
+    <div
+      className="card"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '300px', 
+        width: '300px'}}
+    >
+      card
+    </div>
   )
 }
 
-export default Card
+export default Card;
+
